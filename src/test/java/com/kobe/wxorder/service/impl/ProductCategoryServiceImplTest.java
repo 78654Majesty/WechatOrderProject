57dd973fd44b36083e2cb71e0607d4914b98ab97
fanglingxiao <fanglingxiao@broadtext.com.cn>
2019-07-03 15:01:28 +0800

    develop

--- src/test/java/com/kobe/wxorder/service/impl/ProductCategoryServiceImplTest.java
+++ src/test/java/com/kobe/wxorder/service/impl/ProductCategoryServiceImplTest.java
@@ -4,12 +4,16 @@ import java.util.Date;
 import com.kobe.wxorder.dao.ProductCategoryDao;
 import com.kobe.wxorder.model.ProductCategory;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ProductCategoryServiceImplTest {
 
     @Autowired
